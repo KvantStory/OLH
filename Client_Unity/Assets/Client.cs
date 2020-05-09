4f7e566718a96778c2fa358c60cf860da62556f4
Qliook <QliookdoijnoGT@yandex.ru>
2020-05-09 08:07:30 +0300

    Небольшие изменения

---
+++ Client_Unity/Assets/Client.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Net.Sockets;
+using System.IO;
+using UnityEngine.UI;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class Client : MonoBehaviour
+{
+    private bool socketReady;
+    private TcpClient socket;
+    private NetworkStream stream;
+    private StreamWriter writer;
+    private StreamReader reader;
+
+    private string message;
+
+    public void ConnectedToServer()
+    {
+        if (socketReady)
+            return;
+
+        string host = "";
+        int port = 25666;
+
+        try
+        {
+            socket = new TcpClient(host, port);
+            stream = socket.GetStream();
+            writer = new StreamWriter(stream);
+            reader = new StreamReader(stream);
+            socketReady = true;
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Socket error : " + e.Message);
+        }
+    }
+
+    private void Start()
+    {
+        ConnectedToServer();
+    }
+
+    private void Update()
+    {
+        if (socketReady)
+        {
+            if (stream.DataAvailable)
+            {
+                byte[] buffer = new byte[1024];
+                socket.Client.Receive(buffer);
+
+                message = Encoding.UTF8.GetString(buffer);
+                OnIncomingData();
+                Debug.Log(message);
+
+            }
+        }
+    }
+    private void OnIncomingData()
+    {
+        if (message.Contains("")) { }
+    }
+
+    private void Send(string data)
+    {
+        if (!socketReady)
+            return;
+
+        socket.Client.Send(Encoding.UTF8.GetBytes(data));
+        writer.Flush();
+    }
+
+    private void CloseSocket()
+    {
+        if (!socketReady)
+            return;
+
+        writer.Close();
+        reader.Close();
+        socket.Close();
+        socketReady = false;
+    }
+
+    private void OnApplicationQuit()
+    {
+        CloseSocket();
+    }
+
+    private void OnDisable()
+    {
+        CloseSocket();
+    }
+}
